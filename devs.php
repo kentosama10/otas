956251f3cab6a6a9024bf1a2efced11d684e1f48
Kent Sumbilon <kntsmbln1@gmail.com>
2023-04-15 15:00:30 +0800

    minor changes

--- devs.php
+++ devs.php
@@ -12,6 +12,7 @@ endif;?>
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="shortcut icon" href="img/svci_logo.png">
   <title>The Developers</title>
 
   <style>
@@ -127,8 +128,6 @@ endif;?>
       }
     }
   </style>
-  <title>
-  </title>
   
 </head>
 
