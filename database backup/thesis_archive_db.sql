6c9ac5e87ddbf74e3f056b1fe49dd897eb01e332
Kent Sumbilon <kntsmbln1@gmail.com>
2023-05-04 10:41:54 +0800

    added backup

---
+++ database backup/thesis_archive_db.sql
@@ -0,0 +1,147 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 04, 2023 at 04:41 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `thesis_archive_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_accounts`
+--
+
+CREATE TABLE `admin_accounts` (
+  `id` bigint(20) NOT NULL,
+  `acc_id` bigint(20) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `acc_name` varchar(255) NOT NULL,
+  `acc_type` varchar(255) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admin_accounts`
+--
+
+INSERT INTO `admin_accounts` (`id`, `acc_id`, `email`, `username`, `password`, `acc_name`, `acc_type`, `date`) VALUES
+(1, 1211900327, 'dummy.dummy2@gmail.com', 'admin1', 'a', 'Kent', 'Expert', '2023-05-04 02:31:21');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `uploaded_thesis`
+--
+
+CREATE TABLE `uploaded_thesis` (
+  `id` bigint(20) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `abstract` longtext NOT NULL,
+  `author` varchar(255) NOT NULL,
+  `department` varchar(255) NOT NULL,
+  `program` varchar(255) NOT NULL,
+  `year` year(4) NOT NULL,
+  `date` date NOT NULL,
+  `uploaded_by` varchar(255) NOT NULL,
+  `file_name` varchar(255) NOT NULL,
+  `file_size` int(11) NOT NULL,
+  `file_tmp` varchar(255) NOT NULL,
+  `file_type` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_accounts`
+--
+
+CREATE TABLE `user_accounts` (
+  `id` bigint(20) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `acc_name` varchar(255) NOT NULL,
+  `acc_type` varchar(255) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_accounts`
+--
+
+INSERT INTO `user_accounts` (`id`, `email`, `username`, `password`, `acc_name`, `acc_type`, `date`) VALUES
+(76, 'aozulx@m.com', 'user1', '1', 'Crix', 'End-user', '2022-09-07 08:35:52');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin_accounts`
+--
+ALTER TABLE `admin_accounts`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `acc_id` (`acc_id`),
+  ADD KEY `date` (`date`),
+  ADD KEY `email` (`email`),
+  ADD KEY `username` (`username`);
+
+--
+-- Indexes for table `uploaded_thesis`
+--
+ALTER TABLE `uploaded_thesis`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_accounts`
+--
+ALTER TABLE `user_accounts`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `date` (`date`),
+  ADD KEY `email` (`email`),
+  ADD KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin_accounts`
+--
+ALTER TABLE `admin_accounts`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+
+--
+-- AUTO_INCREMENT for table `uploaded_thesis`
+--
+ALTER TABLE `uploaded_thesis`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `user_accounts`
+--
+ALTER TABLE `user_accounts`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=77;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
